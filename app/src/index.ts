e99bd64f635d01ea095e3549ecce339ceb550024
jinyao <1650091775@qq.com>
2024-10-10 17:20:26 +0800

    add liquidity demo

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,7 +1,11 @@
-import { Brevis, ErrCode, ProofRequest, Prover, TransactionData } from 'brevis-sdk-typescript';
+import { Brevis, ErrCode, ProofRequest, Prover, StorageData, TransactionData } from 'brevis-sdk-typescript';
 import { ethers } from 'ethers';
 
 async function main() {
+   TestLiquidity()
+}
+
+async function TestAge() {
     const prover = new Prover('localhost:33247');
     const brevis = new Brevis('appsdkv2.brevis.network:9094');
 
@@ -88,6 +92,52 @@ async function main() {
         console.log('brevis res', brevisRes);
 
         await brevis.wait(brevisRes.queryKey, 97);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function TestLiquidity() {
+    const prover = new Prover('localhost:33247');
+    const brevis = new Brevis('appsdkv2.brevis.network:9094');
+
+    const proofReq = new ProofRequest();
+    
+    proofReq.addStorage(
+        new StorageData({
+            block_num: 20933863,
+            address: "0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640",
+            slot: "0x0000000000000000000000000000000000000000000000000000000000000004",
+            value: "0x6b1c078ec5bf117e"
+        }),
+    );
+
+    const proofRes = await prover.prove(proofReq);
+    // error handling
+    if (proofRes.has_err) {
+        const err = proofRes.err;
+        switch (err.code) {
+            case ErrCode.ERROR_INVALID_INPUT:
+                console.error('invalid receipt/storage/transaction input:', err.msg);
+                break;
+
+            case ErrCode.ERROR_INVALID_CUSTOM_INPUT:
+                console.error('invalid custom input:', err.msg);
+                break;
+
+            case ErrCode.ERROR_FAILED_TO_PROVE:
+                console.error('failed to prove:', err.msg);
+                break;
+        }
+        return;
+    }
+    console.log('proof', proofRes.proof);
+
+    try {
+        const brevisRes = await brevis.submit(proofReq, proofRes, 1, 11155111, 0, "", "");
+        console.log('brevis res', brevisRes);
+
+        await brevis.wait(brevisRes.queryKey, 11155111);
     } catch (err) {
         console.error(err);
     }

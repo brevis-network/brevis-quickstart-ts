6cb6a4e6b44f09f2c8c801d3d1c88144b07b72dd
jinyao <1650091775@qq.com>
2024-11-11 15:39:00 +0800

    Update v0.3 (#1)
    * update brevis sdk
    
    * update brevis sdk
    
    * update example circuit
    
    * update module name
    
    * fix compilation issue
    
    * update brevis sdk typescript version

--- prover/circuits/circuit_test.go
+++ prover/circuits/circuit_test.go
@@ -1,61 +1,49 @@
-package age
+package circuits
 
 import (
-	"context"
-	"math/big"
 	"testing"
 
 	"github.com/brevis-network/brevis-sdk/sdk"
 	"github.com/brevis-network/brevis-sdk/test"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 func TestCircuit(t *testing.T) {
-	app, err := sdk.NewBrevisApp()
-	check(err)
-	ec, err := ethclient.Dial("https://eth.llamarpc.com")
+	rpc := "RPC_URL"
+	localDir := "$HOME/circuitOut/myBrevisApp"
+	app, err := sdk.NewBrevisApp(1, rpc, localDir)
 	check(err)
 
 	txHash := common.HexToHash(
-		"4a18c7762036fcd4016d9ba74b40a8c3614adf9b6f7c6439c4675f9e828211c8")
-	tx, _, err := ec.TransactionByHash(context.Background(), txHash)
-	check(err)
-	receipt, err := ec.TransactionReceipt(context.Background(), txHash)
-	check(err)
-	from, err := types.Sender(types.NewLondonSigner(tx.ChainId()), tx)
-	check(err)
+		"0x8a7fc50330533cd0adbf71e1cfb51b1b6bbe2170b4ce65c02678cf08c8b17737")
 
-	gtc := big.NewInt(0)
-	gasFeeCap := big.NewInt(0)
-	if tx.Type() == types.LegacyTxType {
-		gtc = tx.GasPrice()
-	} else {
-		gtc = tx.GasTipCap()
-		gasFeeCap = tx.GasFeeCap()
-	}
-
-	app.AddTransaction(sdk.TransactionData{
-		Hash:                txHash,
-		ChainId:             tx.ChainId(),
-		BlockNum:            receipt.BlockNumber,
-		Nonce:               tx.Nonce(),
-		GasTipCapOrGasPrice: gtc,
-		GasFeeCap:           gasFeeCap,
-		GasLimit:            tx.Gas(),
-		From:                from,
-		To:                  *tx.To(),
-		Value:               tx.Value(),
+	app.AddReceipt(sdk.ReceiptData{
+		TxHash: txHash,
+		Fields: []sdk.LogFieldData{
+			{
+				IsTopic:    true,
+				LogPos:     0,
+				FieldIndex: 1,
+			},
+			{
+				IsTopic:    false,
+				LogPos:     0,
+				FieldIndex: 0,
+			},
+		},
 	})
 
-	guest := &AppCircuit{}
-	guestAssignment := &AppCircuit{}
+	appCircuit := &AppCircuit{}
+	appCircuitAssignment := &AppCircuit{}
 
-	circuitInput, err := app.BuildCircuitInput(guest)
+	circuitInput, err := app.BuildCircuitInput(appCircuit)
 	check(err)
 
-	test.ProverSucceeded(t, guest, guestAssignment, circuitInput)
+	///////////////////////////////////////////////////////////////////////////////
+	// Testing
+	///////////////////////////////////////////////////////////////////////////////
+
+	test.ProverSucceeded(t, appCircuit, appCircuitAssignment, circuitInput)
 }
 
 func check(err error) {
--- prover/cmd/main.go
+++ prover/cmd/main.go
@@ -4,20 +4,22 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	age "pancake-prover/circuits"
+
+	"prover/circuits"
 
 	"github.com/brevis-network/brevis-sdk/sdk/prover"
 )
 
 var port = flag.Uint("port", 33247, "the port to start the service at")
 
-// example usage: prover -service="totalfee" -port=33248
 func main() {
 	flag.Parse()
 
-	proverService, err := prover.NewService(&age.AppCircuit{}, prover.ServiceConfig{
+	proverService, err := prover.NewService(&circuits.AppCircuit{}, prover.ServiceConfig{
 		SetupDir: "$HOME/circuitOut",
 		SrsDir:   "$HOME/kzgsrs",
+		RpcURL:   "https://eth.llamarpc.com",
+		ChainId:  1,
 	})
 	if err != nil {
 		fmt.Println(err)
